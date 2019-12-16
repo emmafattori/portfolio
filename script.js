3f9dc423d95495ac32a575ce0a4c6ac6c783e19f
emmafattori <emmalfattori@gmail.com>
2019-12-16 14:43:52 -0500

    mobile menu

---
+++ script.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+
+// 			$('a[href*="#"]').on('click', function(e) {
+//   				e.preventDefault()
+
+//   			$('html, body').animate({
+//    				 scrollTop: $($(this).attr('href')).offset().top,
+//    			 }, 2000,'linear')
+// 			})
+// 			$(".open").on('click', function(){
+// 				$("ul").toggleClass("mobile-nav")
+// 			});
+
+// 			$(".close").on('click', function(){
+// 				$("ul").toggleClass("mobile-nav")
+// 			})
+
+// 		});
